4c8f2483f24e4d423a6d3d638e98b67c234501ee
sutinenpirkko <pirkko.sutinen@sasky.fi>
2023-08-08 08:49:08 +0300

    Find files
    Find files done

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var currentFile in foundFiles)
+    {
+        if (currentFile.EndsWith("sales.json"))
+        {
+            salesFiles.Add(currentFile);
+        }
+    }
+
+    return salesFiles;
+}
+
+var salesFiles = FindFiles("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
